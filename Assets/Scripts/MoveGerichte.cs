845a3711260e3657e420d6865a2905b0f9b23c62
Michelle-P95 <michelle.poetsch@gmx.de>
2021-05-30 00:56:55 +0200

    unnoetige variable deletet

--- Assets/Scripts/MoveGerichte.cs
+++ Assets/Scripts/MoveGerichte.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class MoveGerichte : MonoBehaviour
 {
     public float speed = 5f;
-    public Vector3 playerPosition;
     public string rotationDirection;
 
     // Start is called before the first frame update
