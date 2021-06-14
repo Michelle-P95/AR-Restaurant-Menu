1296038927cde704dd4c47df5b25617e4ae25f55
Markmc17 <64312949+Markmc17@users.noreply.github.com>
2021-06-14 11:17:05 +0200

    Update
    3D Modelle gehen aus bei anderem Button click

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -1,25 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using Vuforia;
 
 public class MenuManager : MonoBehaviour
 {
     string gericht;
+    //public MenuManager mMenumanager;
+    //public GameObject Speisekarte;
+    //private Speisekarte mySpeisekarte;
     public GameObject description;
     public GameObject virtualButtonBeschreibung;
     public GameObject virtualButtonBestellung;
+    String lastGericht;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
 
+        //mySpeisekarte = Speisekarte.GetComponent<Speisekarte>();
         virtualButtonBeschreibung = GameObject.Find("Button_Desciption");
         virtualButtonBestellung = GameObject.Find("Button_Bestellen");
 
         virtualButtonBeschreibung.GetComponent<VirtualButtonBehaviour>().RegisterOnButtonPressed(Info);
         virtualButtonBestellung.GetComponent<VirtualButtonBehaviour>().RegisterOnButtonPressed(Bestellen);
 
+        virtualButtonBeschreibung.GetComponent<VirtualButtonBehaviour>().RegisterOnButtonReleased(InfoReleased);
+        virtualButtonBestellung.GetComponent<VirtualButtonBehaviour>().RegisterOnButtonReleased(BestellenReleased);
+
+        //description = GameObject.Find("description" + mySpeisekarte.lastGericht.GetComponent<String>());
+        description = GameObject.Find("DescriptionPizza");
+        description.SetActive(false);
 
     }
 
@@ -29,16 +42,29 @@ public class MenuManager : MonoBehaviour
         
     }
 
+    public void BestellenReleased(VirtualButtonBehaviour vb) 
+    {
+        Debug.Log("+/Bestellen - Button released");
+    }
+
     public void Info(VirtualButtonBehaviour obj)
     {
         Debug.Log("Info - Button pressed");
-        String lastGericht = Speisekarte.lastGericht.GetComponent<Text>();
+        //description = GameObject.Find("description" + mySpeisekarte.lastGericht.GetComponent<String>());
+        description = GameObject.Find("DescriptionPizza");
+        description.SetActive(true);
 
+    }
+
+    public void InfoReleased(VirtualButtonBehaviour vb)
+    {
+        Debug.Log("Info - Button released");
     }
 
     // Update is called once per frame
     void Update()
     {
-        gericht = Speisekarte.gericht;
+        //mySpeisekarte = Speisekarte.GetComponent<Speisekarte>();
+        
     }
 }
--- Assets/Scripts/Speisekarte.cs
+++ Assets/Scripts/Speisekarte.cs
@@ -4,14 +4,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using Vuforia;
 
+
 public class Speisekarte : MonoBehaviour
 {
-    
+    public Speisekarte mSpeisekarte;
     public String gericht;
     public GameObject gericht3Dmodel;
     public GameObject virtualButton;
     public GameObject lastGericht;
-    
+
 
     // Start is called before the first frame update
     void Start()
@@ -20,30 +21,44 @@ public class Speisekarte : MonoBehaviour
 
         virtualButton.GetComponent<VirtualButtonBehaviour>().RegisterOnButtonPressed(OnButtonPressed);
         virtualButton.GetComponent<VirtualButtonBehaviour>().RegisterOnButtonReleased(OnButtonReleased);
-        
+
         gericht3Dmodel.SetActive(false);
-               
+        //gericht3Dmodel.tag = "Modelle";
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     // Methoden wenn VirtualButton mit finger getriggert wird
     private void OnButtonPressed(VirtualButtonBehaviour obj)
     {
         Debug.Log(gericht + " - Button pressed");
+
         gericht3Dmodel.SetActive(true);
-       
+
     }
 
     public void OnButtonReleased(VirtualButtonBehaviour vb)
     {
         Debug.Log("Button released");
+        if (gericht3Dmodel != GameObject.Find("Burger")) { GameObject.Find("Burger").SetActive(false); }
+        if (gericht3Dmodel != GameObject.Find("Pizza")) { GameObject.Find("Pizza").SetActive(false); }
+        if (gericht3Dmodel != GameObject.Find("Hotdog")) { GameObject.Find("Hotdog").SetActive(false); }
+        if (gericht3Dmodel != GameObject.Find("Pommes")) { GameObject.Find("Pommes").SetActive(false); }
+    }
 
-       
+    public void turnOff()
+    {
+        gericht3Dmodel.SetActive(false);
+    }
+
+    public void getLast()
+    {
+            
     }
 
 }
