d297e016f32c46096323b168f8cd4be181c0d6a2
MVVN <Minh.Vu@Student.HTW-Berlin.de>
2021-06-15 17:53:37 +0200

    Description + Gericht"aktivierung"
    Description working;
    Just one Model active at one time;
    Bug with changeButtonPosition() in Speisekarte.cs

--- Assets/Scripts/Speisekarte.cs
+++ Assets/Scripts/Speisekarte.cs
@@ -6,21 +6,32 @@ using Vuforia;
 
 public class Speisekarte : MonoBehaviour
 {
-    
     public String gericht;
     public GameObject gericht3Dmodel;
     public GameObject virtualButton;
-    
+    public GameObject[] otherDishButtons;
+    public GameObject blackBackground;
+
+    private GameObject description;
+    private Boolean dishActive;
+    private Transform backButtonPosition;
+    private Transform originalButtonPosition;
+
     // Start is called before the first frame update
     void Start()
     {
         virtualButton = GameObject.Find("VirtualButton_" + gericht);
+        description = GameObject.Find(gericht + "_Description");
+        backButtonPosition = GameObject.Find("BackButtonPosition").transform;
+        originalButtonPosition = virtualButton.transform;
 
         virtualButton.GetComponent<VirtualButtonBehaviour>().RegisterOnButtonPressed(OnButtonPressed);
         virtualButton.GetComponent<VirtualButtonBehaviour>().RegisterOnButtonReleased(OnButtonReleased);
 
         gericht3Dmodel.SetActive(false);
- 
+        blackBackground.SetActive(false);
+        description.SetActive(false);
+        dishActive = false;
     }
 
     // Update is called once per frame
@@ -32,14 +43,65 @@ public class Speisekarte : MonoBehaviour
     // Methoden wenn VirtualButton mit finger getriggert wird
     private void OnButtonPressed(VirtualButtonBehaviour obj)
     {
-        Debug.Log(gericht + " - Button pressed");
-        gericht3Dmodel.SetActive(true);
-       
+        if (dishActive)
+        {
+            gericht3Dmodel.SetActive(false);
+            blackBackground.SetActive(false);
+            description.SetActive(false);
+            dishActive = false;
+            // changeButtonPosition();
+            activateDeactivateOtherButtons();
+        }
+        else
+        {
+            if (virtualButton.activeSelf)
+            {
+                gericht3Dmodel.SetActive(true);
+                blackBackground.SetActive(true);
+                description.SetActive(true);
+                dishActive = true;
+                // changeButtonPosition();
+                activateDeactivateOtherButtons();
+            }
+        }
     }
 
     public void OnButtonReleased(VirtualButtonBehaviour vb)
     {
-        Debug.Log("Button released");
-       
+        // Debug.Log("Button released");
+
+    }
+
+    private void activateDeactivateOtherButtons()
+    {
+        if (dishActive)
+        {
+            for (int i = 0; i < otherDishButtons.Length; i++)
+            {
+                otherDishButtons[i].SetActive(false);
+            }
+        }
+        else
+        {
+            for (int i = 0; i < otherDishButtons.Length; i++)
+            {
+                otherDishButtons[i].SetActive(true);
+            }
+        }
+    }
+
+    // TODO: funktioniert noch nicht (Buttons bleiben an falscher Position bestehen)
+    // um aktiven Button des Gerichts rechts neben das 3d Model zu bewegen wenn Gericht aktiv
+    // bei Deaktivierung wird Btn wieder zurück an Originalposition gesetzt
+    private void changeButtonPosition()
+    {
+        if (dishActive)
+        {
+            virtualButton.transform.position = backButtonPosition.position;
+        }
+        else
+        {
+            virtualButton.transform.position = originalButtonPosition.position;
+        }
     }
 }
---
+++ Assets/Scripts/SpeisekartenManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class SpeisekartenManager : MonoBehaviour
+{
+    public GameObject[] dishesArray;
+    public List<Dish> dishes;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dishesArray = GameObject.FindGameObjectsWithTag("dish");
+        for(int i = 0; i < dishesArray.Length; i++) {
+            dishes.Add(new Dish(dishesArray[i].name,
+            dishesArray[i], 
+            GameObject.Find("VirtualButton_" + dishesArray[i].name)));
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+
+/////////////////////////////////////////////////////////////
+/////                     DISH CLASS                    /////
+/////////////////////////////////////////////////////////////
+
+
+public class Dish
+{
+    public string name;
+    public GameObject dish3dModel;
+    public GameObject dishVirtualButton;
+
+    public Dish(string name, GameObject model, GameObject button) {
+        this.name = name;
+        this.dish3dModel = model;
+        this.dishVirtualButton = button;
+        DishSetUp();
+    }
+
+    private void DishSetUp() {
+        dishVirtualButton.GetComponent<VirtualButtonBehaviour>().RegisterOnButtonPressed(OnButtonPressed);
+        dishVirtualButton.GetComponent<VirtualButtonBehaviour>().RegisterOnButtonReleased(OnButtonReleased);
+        dish3dModel.SetActive(false);
+    }
+
+    private void OnButtonPressed(VirtualButtonBehaviour obj)
+    {
+        if(dish3dModel.activeSelf) {
+            dish3dModel.SetActive(false);
+        } else {
+           dish3dModel.SetActive(true); 
+        }              
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        Debug.Log("Button released");       
+    }
+}
