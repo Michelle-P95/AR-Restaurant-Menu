0c8612b02406b2bfaa2594152757584304932a94
Markmc17 <64312949+Markmc17@users.noreply.github.com>
2021-06-19 21:25:38 +0200

    Translation Button
    Überschrift lässt sich ändern

---
+++ Assets/Scripts/TextPlanes.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextPlanes : MonoBehaviour
+{
+    public GameObject germanText;
+    public GameObject englishText;
+    public GameObject Layer;
+    public string text;
+    public bool isGerman;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        text = Layer.name;
+        englishText.SetActive(false);
+        germanText.SetActive(true);
+        isGerman = true;
+    }
+
+    public void changeLang() 
+    {
+        if (isGerman == true)
+        {
+            germanText.SetActive(false);
+            englishText.SetActive(true);
+            isGerman = false;
+        }
+        else if (isGerman == false) 
+        {
+            englishText.SetActive(false);
+            germanText.SetActive(true);
+            isGerman = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
