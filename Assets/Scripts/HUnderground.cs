51b3680b2db9e652f2fc5db55cc74830b5190ec4
Markmc17 <64312949+Markmc17@users.noreply.github.com>
2021-06-27 11:27:05 +0200

    update 3
    3

---
+++ Assets/Scripts/HUnderground.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HUnderground : MonoBehaviour
+{
+    public Language language;
+    public GameObject underground;
+    // Start is called before the first frame update
+    void Start()
+    {
+        underground.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (language.isGerman == true)
+        {
+            underground.SetActive(false);
+        }
+        else if (language.isGerman == false)
+        {
+            underground.SetActive(true);
+        }
+    }
+}
